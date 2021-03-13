fe197d8c73949c589a9d96d1a991bb112d8a20ce
Vita from Taiwan <71432509+vitalin33@users.noreply.github.com>
2021-03-13 19:08:07 +0800

    Update base.php

--- base.php
+++ base.php
@@ -47,12 +47,11 @@ $uploadimg=[
 class DB{
 
     protected $table;
-    protected $dsn="mysql:host=localhost;dbname=s1090414;charset=utf8";
-    protected $pdo;
+     protected $pdo;
 
     function __construct($table){
         $this->table=$table;
-        $this->pdo=new PDO($this->dsn,'s1090414','s1090414');
+       
 
     }
 
